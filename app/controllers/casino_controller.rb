2754135f81b003c8af1f98623c8b99ac4106a4a0
Daniel Buttig <daniel.buttig@gmail.com>
2025-10-17 11:34:51 -0500

    Implements a banking feature
    This commit implements a banking tab which handles the funds for the session. It
    allows the user to cash out, that is, transfer funds from the funds into a player
    bank. It also allows the user to transfer 10 from their player bank into the
    session funds.

--- app/controllers/casino_controller.rb
+++ app/controllers/casino_controller.rb
@@ -1,4 +1,6 @@
 class CasinoController < ApplicationController
   def index
+    session[:funds] ||= 10
+    session[:player_bank] ||= 0
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resource :slots, only: [:show] do
     post :spin, on: :collection
   end
+  resource :bank, only: [:show] do
+    post :cash_out, on: :collection
+    post :deposit, on: :collection
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
