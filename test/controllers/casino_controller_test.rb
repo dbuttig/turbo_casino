1fedfd66966ea36b64b57b95b435b93205a2de0c
Daniel Buttig <daniel.buttig@gmail.com>
2025-10-17 12:59:00 -0500

    Refactor
    Refactors the slots_controller so that it could be better tested in the future.
    The refactor also reduces the complexity and improves readability of the primary
    method.

--- test/controllers/casino_controller_test.rb
+++ test/controllers/casino_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CasinoControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get casino_index_url
+    get casino_path
     assert_response :success
   end
 end
--- test/controllers/slots_controller_test.rb
+++ test/controllers/slots_controller_test.rb
@@ -2,7 +2,17 @@ require "test_helper"
 
 class SlotsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get slots_show_url
+    get slots_path
+    assert_response :success
+  end
+
+  test "should post spin" do
+    post spin_slots_path
+    assert_response :redirect
+  end
+
+  test "should spin with turbo stream" do
+    post spin_slots_path, headers: { "Accept" => "text/vnd.turbo-stream.html" }
     assert_response :success
   end
 end
